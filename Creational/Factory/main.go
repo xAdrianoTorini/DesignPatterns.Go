6f40372dc13fd6a11e4237d1dc489e792bc3fe2e
Adriano Ricardo Felippe Torini <adriano.torini@inlog.com.br>
2020-10-17 16:59:22 -0300

    Added Factory

---
+++ Creational/Factory/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ak47, _ := getGun("ak47")
+	maverick, _ := getGun("maverick")
+	printDetails(ak47)
+	printDetails(maverick)
+}
+
+func printDetails(g iGun) {
+	fmt.Printf("Gun: %s", g.getName())
+	fmt.Println()
+	fmt.Printf("Power: %d", g.getPower())
+	fmt.Println()
+}
